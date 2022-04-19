a312d45fcd09aa19abb09fbcafd88f8962e8af8c
caneersahin <caneersahin77@gmail.com>
2022-04-19 17:01:12 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { Icon } from 'react-icons-kit'
+// import { ic_keyboard_arrow_right } from 'react-icons-kit/icomoon/ic_keyboard_arrow_right'
+import {cogs} from 'react-icons-kit/icomoon/cogs'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Web sitem güncelleniyor.<i class="fas fa-heart"></i></h1>
+        <div className='mb-5 ' style={{width: 60, height: 60}}>
+            <Icon className='text-info ' size={'100%'} icon={cogs}/>
+        </div> 
+        <h3> İlgi ve anlayışınız için teşekkür ederim!</h3>
+        <div >
+          <h4>Caner Şahin</h4>
+          <hr/>
+
+
+            <i className="bi bi-laptop  text-info"></i>  Frontend Developer   <br></br>
+            <i class="bi bi-telephone text-info"></i><a href="tel:+905313277914" style={{color: "white", textDecorationLine: "none"}}>  +905313277914</a> <br></br>
+            <i class="bi bi-envelope-check  text-info"></i><a href = "mailto:caneersahin77@gmail.com" style={{color: "white", textDecorationLine: "none"}}>  caneersahin77@gmail.com</a>
+
+        </div>
       </header>
     </div>
   );
